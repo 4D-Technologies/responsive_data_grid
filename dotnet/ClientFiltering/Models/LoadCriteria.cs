3a6d2f6f2c435cf1b8fa7b881c6b12f335767783
James Hancock <jhancock@cadlearning.com>
2022-08-18 15:01:16 -0400

    Fixes bugs in datetime serialization

--- dotnet/ClientFiltering/Models/LoadCriteria.cs
+++ dotnet/ClientFiltering/Models/LoadCriteria.cs
@@ -1,162 +1,170 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Reflection;
 using System.Runtime.Serialization;
 
 using ClientFiltering.Enums;
 
-namespace ClientFiltering.Models
+namespace ClientFiltering.Models;
+
+/// <summary>
+/// Load Crtieria for api calls that return lists
+/// </summary>
+[DataContract]
+public record LoadCriteria
 {
     /// <summary>
-    /// Load Crtieria for api calls that return lists
+    /// How far to skip into the records? (for paging)
     /// </summary>
-    [DataContract]
-    public record LoadCriteria
+    [DataMember]
+    public int? Skip { get; init; }
+    /// <summary>
+    /// How many records to take (for paging)
+    /// </summary>
+    [DataMember]
+    public int? Take { get; init; }
+    /// <summary>
+    /// Any filter criteria to apply to the results
+    /// </summary>
+    [DataMember]
+    public IEnumerable<FilterCriteria>? FilterBy { get; init; }
+    /// <summary>
+    /// Any ordering criteria to apply to the results
+    /// </summary>
+    [DataMember]
+    public IEnumerable<OrderCriteria>? OrderBy { get; init; }
+
+
+    public static LoadCriteria FromExpressions<TSource>(Expression<Func<TSource, bool>>? filterBy = null, int? skip = null, int? take = null, params OrderCriteria[] orderBy)
+        where TSource : class
     {
-        /// <summary>
-        /// How far to skip into the records? (for paging)
-        /// </summary>
-        [DataMember]
-        public int? Skip { get; init; }
-        /// <summary>
-        /// How many records to take (for paging)
-        /// </summary>
-        [DataMember]
-        public int? Take { get; init; }
-        /// <summary>
-        /// Any filter criteria to apply to the results
-        /// </summary>
-        [DataMember]
-        public IEnumerable<FilterCriteria>? FilterBy { get; init; }
-        /// <summary>
-        /// Any ordering criteria to apply to the results
-        /// </summary>
-        [DataMember]
-        public IEnumerable<OrderCriteria>? OrderBy { get; init; }
+        var filters = new List<FilterCriteria>();
+
+        if (filterBy != null)
+            ParseFilter(filterBy, ref filters);
 
 
-        public static LoadCriteria FromExpressions<TSource>(Expression<Func<TSource, bool>>? filterBy = null, int? skip = null, int? take = null, params OrderCriteria[] orderBy)
-            where TSource : class
+        return new LoadCriteria
         {
-            var filters = new List<FilterCriteria>();
+            FilterBy = filters,
+            OrderBy = orderBy,
+            Skip = skip,
+            Take = take
+        };
+    }
 
-            if (filterBy != null)
-                ParseFilter(filterBy, ref filters);
+    private static void ParseFilter(Expression filterBy, ref List<FilterCriteria> filters, Operators op = Operators.And)
+    {
 
 
-            return new LoadCriteria
-            {
-                FilterBy = filters,
-                OrderBy = orderBy,
-                Skip = skip,
-                Take = take
-            };
+        if (filterBy is LambdaExpression expression)
+        {
+            ParseFilter(expression.Body, ref filters, op);
+            return;
         }
 
-        private static void ParseFilter(Expression filterBy, ref List<FilterCriteria> filters, Operators op = Operators.And)
+
+        if (filterBy is BinaryExpression be)
         {
-
-
-            if (filterBy is LambdaExpression expression)
+            if (be.NodeType == ExpressionType.AndAlso || be.NodeType == ExpressionType.And)
             {
-                ParseFilter(expression.Body, ref filters, op);
+                ParseFilter(be.Left, ref filters, Operators.And);
+                ParseFilter(be.Right, ref filters, Operators.And);
+                return;
+            }
+            else if (be.NodeType == ExpressionType.OrElse || be.NodeType == ExpressionType.Or)
+            {
+                ParseFilter(be.Left, ref filters, Operators.Or);
+                ParseFilter(be.Right, ref filters, Operators.Or);
                 return;
             }
 
-                
-            if (filterBy is BinaryExpression be)
+
+            var left = (be.Left as MemberExpression)!;
+
+            object? value = null;
+            if (be.Right is ConstantExpression ce)
             {
-                if (be.NodeType == ExpressionType.AndAlso || be.NodeType == ExpressionType.And)
-                {
-                    ParseFilter(be.Left, ref filters, Operators.And);
-                    ParseFilter(be.Right, ref filters, Operators.And);
-                    return;
-                } 
-                else if (be.NodeType == ExpressionType.OrElse || be.NodeType == ExpressionType.Or)
-                {
-                    ParseFilter(be.Left, ref filters, Operators.Or);
-                    ParseFilter(be.Right, ref filters, Operators.Or);
-                    return;
-                }
-
-
-                var left = (be.Left as MemberExpression)!;
-
-                object? value = null;
-                if (be.Right is ConstantExpression ce)
-                {
-                    value = ce.Value;
-                } else if (be.Right is MemberExpression me)
-                {
-                    value = Expression.Lambda(me).Compile().DynamicInvoke();
-                }
-
-                var logic = be.ToLogic();
-
-
-                filters.Add(new FilterCriteria
-                {
-                    FieldName = left.Member.Name,
-                    LogicalOperator = logic,
-                    Values = new[] { value == null ? null : Convert.ToString(value, System.Globalization.CultureInfo.InvariantCulture) },
-                    Op = op
-                });
-
-                return;
+                value = ce.Value;
             }
-
-            bool isParentNot = false;
-            if (filterBy.NodeType == ExpressionType.Not)
+            else if (be.Right is MemberExpression me)
             {
-                filterBy = ((UnaryExpression)filterBy).Operand;
-                isParentNot = true;
+                value = Expression.Lambda(me).Compile().DynamicInvoke();
             }
 
-            if (filterBy is MethodCallExpression mce)
-            {                
-                var logic = mce.ToLogic(isParentNot);
+            var logic = be.ToLogic();
 
-                switch(mce.Method.Name.ToLower())
-                {
-                    case "endswith":
-                    case "startswith":
-                        var value = ((ConstantExpression)mce.Arguments.First()).Value;
-                        filters.Add(new FilterCriteria
-                        {
-                            FieldName = ((MemberExpression) mce.Object!).Member.Name,
-                            LogicalOperator = logic,
-                            Op = op,
-                            Values = new[] { value == null ? null : Convert.ToString(value, System.Globalization.CultureInfo.InvariantCulture) }
-                        });
-                        break;
-                    case "contains":
-                        var valueExpression = mce.Arguments.FirstOrDefault(a => a.NodeType == ExpressionType.MemberAccess &&
-                                                            a is MemberExpression ce &&
-                                                            ce.Expression is ConstantExpression);
 
-                        object? values = null;
-                        if (valueExpression != null)
-                            values = Expression.Lambda(valueExpression).Compile().DynamicInvoke();
+            filters.Add(new FilterCriteria
+            {
+                FieldName = left.Member.Name,
+                LogicalOperator = logic,
+                Values = new[] { GetValue(value) },
+                Op = op
+            });
 
-                        filters.Add(new FilterCriteria
-                        {
-                            FieldName = mce.Arguments.Where(a => a.NodeType == ExpressionType.MemberAccess &&
-                                                            a is MemberExpression me &&
-                                                            me.Expression is ParameterExpression).Cast<MemberExpression>().First().Member.Name,
-                            LogicalOperator = logic,
-                            Values = values == null ? Array.Empty<string?>() : values is IEnumerable<object?> ve ? ve?.Select(v => v == null ? null : Convert.ToString(v, System.Globalization.CultureInfo.InvariantCulture)) ?? Array.Empty<string?>() : new[] { values.ToString() },
-                            Op = op
-                        });
-                        break;
-                }
+            return;
+        }
 
+        bool isParentNot = false;
+        if (filterBy.NodeType == ExpressionType.Not)
+        {
+            filterBy = ((UnaryExpression)filterBy).Operand;
+            isParentNot = true;
+        }
+
+        if (filterBy is MethodCallExpression mce)
+        {
+            var logic = mce.ToLogic(isParentNot);
+
+            switch (mce.Method.Name.ToLower())
+            {
+                case "endswith":
+                case "startswith":
+                    var value = ((ConstantExpression)mce.Arguments.First()).Value;
+                    filters.Add(new FilterCriteria
+                    {
+                        FieldName = ((MemberExpression)mce.Object!).Member.Name,
+                        LogicalOperator = logic,
+                        Op = op,
+                        Values = new[] { GetValue(value) }
+                    });
+                    break;
+                case "contains":
+                    var valueExpression = mce.Arguments.FirstOrDefault(a => a.NodeType == ExpressionType.MemberAccess &&
+                                                        a is MemberExpression ce &&
+                                                        ce.Expression is ConstantExpression);
+
+                    object? values = null;
+                    if (valueExpression != null)
+                        values = Expression.Lambda(valueExpression).Compile().DynamicInvoke();
+
+                    filters.Add(new FilterCriteria
+                    {
+                        FieldName = mce.Arguments.Where(a => a.NodeType == ExpressionType.MemberAccess &&
+                                                        a is MemberExpression me &&
+                                                        me.Expression is ParameterExpression).Cast<MemberExpression>().First().Member.Name,
+                        LogicalOperator = logic,
+                        Values = values == null ? Array.Empty<string?>() : values is IEnumerable<object?> ve ? ve?.Select(v => GetValue(v)) ?? Array.Empty<string?>() : new[] { GetValue(values) },
+                        Op = op
+                    });
+                    break;
             }
-            
 
         }
+
+
+    }
+    private static string? GetValue(object? value)
+    {
+        return value switch
+        {
+            null => null,
+            DateTimeOffset dt => dt.ToString("o", CultureInfo.InvariantCulture),
+            DateTime dt => dt.ToString("o", CultureInfo.InvariantCulture),
+            _ => Convert.ToString(value, CultureInfo.InvariantCulture)
+        };
     }
 }
